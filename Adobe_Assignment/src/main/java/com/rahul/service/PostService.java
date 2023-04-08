d351cc4aca185919a20f8b9950c603f1831810e0
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-04-08 11:12:06 +0530

    added userService and PostService

--- Adobe_Assignment/src/main/java/com/rahul/service/PostService.java
+++ Adobe_Assignment/src/main/java/com/rahul/service/PostService.java
@@ -1,11 +1,20 @@
 package com.rahul.service;
-
 import com.rahul.model.Post;
+
 
 public interface PostService {
 
+	public Post createPost(Post post);
 	
+	public Post  getPostById(Long postId);
 	
+	public Post updatePostById(long postId, Post post);
+	
+	public String deletePostById(Long postId);
+	
+	public Post likePost(Long postId);
+	
+	public Post unlikePost(Long postId);
 	
 	
 }
--- Adobe_Assignment/src/main/java/com/rahul/service/UserService.java
+++ Adobe_Assignment/src/main/java/com/rahul/service/UserService.java
@@ -1,5 +1,23 @@
 package com.rahul.service;
 
+import java.util.List;
+
+import com.rahul.exception.UserNotFoundException;
+import com.rahul.model.User;
+
 public interface UserService {
 
+	
+	public User createUser(User user);
+	
+	public User getUserById(Long userId);
+	
+	public User updateUserById(Long userId, User user);
+	
+	public User deleteUserById(Long userId) throws UserNotFoundException;
+	
+	public Long getTotalNumberOfUser();
+	
+	public List<User> getTopActiveUsers(); 
+	
 }
